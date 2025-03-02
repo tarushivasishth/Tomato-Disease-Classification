f5330ef6fc30559a3de3cd69328be51112b37aac
Tarushi Vasishth <tarushivasishth@gmail.com>
2025-03-02 11:21:24 +0530

    change in main.py

--- main.py
+++ main.py
@@ -58,4 +58,7 @@ if __name__ == "__main__":
     import os
     import sys
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    uvicorn.run(app, host="localhost", port=5000)
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    # uvicorn.run(app, host="localhost", port=5000)
